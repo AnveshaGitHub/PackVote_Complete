6db8552671aa9552ba5bdc8b95ef12aa8cc5c193
samriddhi-kumari-28 <samriddhi28kumari@gmail.com>
2026-04-23 01:59:27 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -705,4 +705,4 @@ def generate_tasks():
 if __name__ == '__main__':
     print("🌍 PackVote starting...")
     print("📍 http://127.0.0.1:5000")
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=False)
